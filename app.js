a69a42e12fef67d10a280a51fcfaf68bec779b5a
ChantyHe90 <ceru@gmx.net>
2020-06-26 11:00:20 +0200

    added vars

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-console.log(process.env.NODE_ENV)
+console.log(process.env.API_URL)
 const http = require("http");
-const NODE_ENV = process.env.NODE_ENV || "[NODE_ENV]";
+const API_URL = process.env.API_URL || "[API_URL]";
+
+// const QUOTE_SERVER_NAME = process.env.QUOTE_SERVER_NAME || "[QUOTE_SERVER_NAME]";
+const PROTOCOL = process.env.PROTOCOL || false;
+
 const httpServer = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
@@ -15,9 +19,21 @@ const httpServer = http.createServer((req, res) => {
     
     </style>
     <body>
-    <h1>You use: ${NODE_ENV}</h1>
+    <h1 id="text">You use: ${API_URL}</h1>
     <pre style="color: red;" id="error-message"></pre>
-
+    <script>
+    const protocol = ${PROTOCOL} || location.protocol;
+    const quote_url = protocol + "//${PROTOCOL}";
+    fetch(quote_url + "?t=" + Date.now()).then((response) => {
+      return response
+    }, (error) => {
+      document.getElementById("error-message").textContent = "FAILED: GET " + quote_url;
+    }).then((quote) => {
+      document.getElementById("text").textContent = "You connected ${API_URL}"
+    }, (error) => {
+      document.getElementById("error-message").textContent = "REQUESTED: GET " + quote_url + "\\nFAILED: " + error;
+    });
+    </script>
     </body>
     </html>`);
 });
